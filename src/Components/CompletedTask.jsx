a77845272101ee73ac4e16ba1e4f0735dc71e4cb
Shabin-k <helloshabink@gmail.com>
2022-11-11 20:58:09 +0530

    add key

--- src/Components/CompletedTask.jsx
+++ src/Components/CompletedTask.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 function CompletedTask({toDos}) {
   return (
-    <div>
-        {toDos.map((obj)=>{
+    <div className='Com-task'>
+     <div>
+        {toDos.map((obj, index)=>{
           if(obj.status){
-            return(<h1>{obj.text}</h1>)
+            return(<div key={index}><h1>{obj.text}</h1></div>
+            )
           }
           return null
-         })}
+         })}    
+     </div>
     </div>
   )
 }
